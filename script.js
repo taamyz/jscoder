f75420a4c854935deefd00f67e4bf6c9d1668b77
taamyz <44944423+taamyz@users.noreply.github.com>
2023-06-16 03:19:52 +0000

    taskk1

---
+++ script.js
@@ -0,0 +1,7 @@
+var quote1 = prompt("Escreva uma frase");
+var quote2 = prompt("Agora, escreva algo para combinar com ela:");
+
+var result = quote1 + " " + quote2;
+
+
+alert("Sua frase final é:" + result);
